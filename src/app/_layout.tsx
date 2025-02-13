420c7138a73e9636cd19eb411097323241746f76
Ogoigbe1 <ogoigbe12@gmail.com>
2025-02-13 06:58:02 +0100

    init

---
+++ src/app/_layout.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+import 'react-native-reanimated';
+
+
+
+export default function RootLayout() {
+  
+
+  return (
+      <>
+        <StatusBar style="auto" />
+        <Text>Amazon Clone</Text>
+      </>
+  );
+}

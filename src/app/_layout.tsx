5d5d8af0a1a9635ccd42e63d20281ca6d408a995
Ogoigbe1 <ogoigbe12@gmail.com>
2025-02-13 07:56:09 +0100

    made changes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,23 @@
-
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import 'react-native-reanimated';
-
+import { TamaguiProvider } from 'tamagui';
+import { tamaguiConfig } from '@/tamagui.config'
+import { Stack } from 'expo-router';
 
 
 export default function RootLayout() {
   
 
   return (
-      <>
+    <>
+    {/* <TamaguiProvider config={tamaguiConfig}> */}
         <StatusBar style="auto" />
-        <Text>Amazon Clone</Text>
-      </>
+        <Stack screenOptions={{headerShown: false}}>
+          <Stack.Screen name='(tabs)'/>
+        </Stack>
+        {/* </TamaguiProvider> */}
+        </>
   );
 }
